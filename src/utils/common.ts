5d8539d237f09d635892f4b1b5be98c2d1fecd1a
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-08-29 23:45:01 +0530

    fix: remove common helper function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,14 +2,3 @@ export function getErrorMessage(error: unknown) {
   if (error instanceof Error) return error.message
   return String(error)
 }
-
-export function modifyRoomData(data: any) {
-  const rooms = data.map((room: any) => {
-    return {
-      ...room,
-      costPerDay: room.costPerDay.toNumber()
-    }
-  })
-
-  return rooms
-}
