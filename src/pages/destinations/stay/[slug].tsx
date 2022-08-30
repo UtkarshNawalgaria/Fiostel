d7fee7ac3c1d8c367f315d27cfd294a62f226ab0
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2022-08-30 21:52:56 +0530

    fix: styling inidividual room rows

--- src/pages/destinations/stay/[slug].tsx
+++ src/pages/destinations/stay/[slug].tsx
@@ -1,6 +1,6 @@
-import { Prisma } from '@prisma/client';
+import { Prisma } from '@prisma/client'
 import React from 'react'
-import { modifyRoomData } from '../../../utils/common'
+import { MdArrowRightAlt } from 'react-icons/md'
 import prisma from '../../../utils/prisma'
 
 const roomSelectFields = {
@@ -38,46 +38,71 @@ const staySelectFields = {
 }
 
 const stay = Prisma.validator<Prisma.StayArgs>()({
-  select: staySelectFields
+  select: staySelectFields,
 })
 
 const room = Prisma.validator<Prisma.RoomArgs>()({
-  select: roomSelectFields
+  select: roomSelectFields,
 })
 
+type costPerDay = {
+  costPerDay: number
+}
 type Stay = Prisma.StayGetPayload<typeof stay>
-type Room = Prisma.RoomGetPayload<typeof room>
+type Room = Prisma.RoomGetPayload<typeof room> & costPerDay
 
 const RoomList: React.FC<{ rooms: Room[]; stay: Stay }> = ({ stay, rooms }) => {
-
   if (rooms.length == 0) {
     return <div>No Rooms Available</div>
   }
-  console.log(rooms)
+  
   return (
-    <section className="mb-5 container mx-auto max-w-7xl md:my-10">
-      <div className="">
-        <img
-          src={stay.media?.images[0].url}
-          alt={stay.name}
-          className="w-full h-[300px] md:h-[500px] object-cover rounded-lg"
-        />
-      </div>
-      <div className="flex flex-col md:flex-row">
+    <>
+      <section className="mb-5 container mx-auto max-w-7xl md:my-10">
         <div>
-          <h1 className='text-5xl font-bold mt-5'>{stay.name}</h1>
+          <img
+            src={stay.media?.images[0].url}
+            alt={stay.name}
+            className="w-full h-[300px] md:h-[500px] object-cover md:rounded-lg"
+          />
         </div>
-      </div>
-      <div className="room-list">
-        {rooms.map((room) => {
-          return (
-            <div key={room.slug}>
-              <h3>{room.name}</h3>
-            </div>
-          )
-        })}
-      </div>
-    </section>
+        <div className="flex flex-col p-4 md:flex-row md:p-0">
+          <div>
+            <h1 className="text-5xl font-bold mt-5">{stay.name}</h1>
+          </div>
+        </div>
+      </section>
+      <section className="container mx-auto max-w-7xl p-4 flex flex-col-reverse gap-4 md:flex-row md:my-10 md:p-0">
+        <div className="room-list flex flex-col md:w-3/4">
+          {rooms.map((room) => {
+            return (
+              <div key={room.slug} className="flex flex-col mb-10 md:flex-row">
+                <div className="w-[300px]">
+                  <img
+                    src={room.media?.images[0].url}
+                    alt={room.name}
+                    className="md:h-[200px] md:rounded-tl-lg md:rounded-bl-lg"
+                  />
+                </div>
+                <div className="w-full border-[#e7e7e7e] flex flex-col justify-between md:px-4 md:border-y md:border-r md:rounded-tr-lg md:rounded-br-lg">
+                  <div className="flex justify-between w-full">
+                    <h3 className="font-bold text-xl">{room.name}</h3>
+                    <div className="font-bold">Rs {room?.costPerDay}</div>
+                  </div>
+                  <div className='flex justify-end'>
+                    <button className="inline-block cursor-pointer bg-[#facc15] text-white my-4 font-semibold px-6 py-2 rounded-md mt-5 hover:text-[#facc15] hover:bg-white border-2 border-[#facc15] focus:outline-none transition ease-in-out duration-100">
+                      <span>Book Room</span>
+                      <MdArrowRightAlt className="inline-block ml-2 text-xl" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className="md:w-1/4">Calendar</div>
+      </section>
+    </>
   )
 }
 
@@ -91,17 +116,24 @@ export async function getStaticProps({ params }: { params: { slug: string } }) {
     select: staySelectFields,
   })
 
-  const rooms = await prisma.room.findMany({
-    where: {
-      stayId: stay?.id,
-    },
-    select: roomSelectFields
-  })
+  const rooms = await prisma.room
+    .findMany({
+      where: {
+        stayId: stay?.id,
+      },
+      select: roomSelectFields,
+    })
+    .then((data) => {
+      return data.map((room) => ({
+        ...room,
+        costPerDay: room.costPerDay.toNumber(),
+      }))
+    })
 
   return {
     props: {
       stay,
-      rooms: rooms.length > 0 ? modifyRoomData(rooms) : [],
+      rooms: rooms.length > 0 ? rooms : [],
     },
   }
 }
